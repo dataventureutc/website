fa9c95c5213a4582cd832b07a20d7f0aedc47e1e
Sylvain M <sylvain.marchienne@gmail.com>
2017-01-22 16:49:21 +0100

    Landing page

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 const elixir = require('laravel-elixir');
-
 require('laravel-elixir-vue-2');
+
+var gulp = require('gulp'),
+    notify = require('gulp-notify'),
+    del = require('del'),
+    concat = require('gulp-concat'),
+    sourcemaps = require('gulp-sourcemaps');
+    sass = require('gulp-sass'),
+    rename = require("gulp-rename"),
+    cleanCSS = require('gulp-clean-css');
+
+var src = 'resources';
 
 /*
  |--------------------------------------------------------------------------
@@ -14,4 +24,48 @@ require('laravel-elixir-vue-2');
  */
 
 elixir(mix => {
+  mix.task('default')
+});
+
+gulp.task('default', ['clean'], function() {
+  gulp.start('sources');
+  gulp.start('materialize');
+});
+
+gulp.task('clean', function() {
+    return del(['public/assets']);
+});
+
+gulp.task('watch', function() {
+  gulp.watch([src + '/**/*', '!' + src + '/assets/sass/{materialize.scss,components/**/*}'], ['sources']);
+  gulp.watch(src + '/assets/sass/{materialize.scss,components/**/*}', ['materialize']);
+});
+
+gulp.task('materialize', function() {
+
+  del(['public/assets/css/materialize.min.css']);
+
+  gulp.src(src + '/assets/sass/materialize.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(cleanCSS())
+  .pipe(sourcemaps.write())
+  .pipe(rename('materialize.min.css'))
+  .pipe(gulp.dest('public/assets/css'));
+
+});
+
+gulp.task('sources', function() {
+
+  gulp.src(src + '/assets/sass/style.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(cleanCSS())
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('public/assets/css'));
+
+  // assets
+  gulp.src([src + '/assets/**/*', '!' + src + '/assets/{sass,sass/**}'])
+  .pipe(gulp.dest('public/assets'))
+
 });
---
+++ resources/assets/js/script.js
@@ -0,0 +1,8 @@
+$(function(){
+  $(".element").typed({
+    strings: ["de machine learning", "de dataviz", "de réseaux, graphes"],
+    typeSpeed: 90,
+    backDelay: 1500,
+    loop:true,
+  });
+});
